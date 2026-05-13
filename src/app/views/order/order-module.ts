6528d9d63a16989f31887bfbe805b7b5ec8e9ebc
Sidorov Vitaly <vit_s71@yahoo.com>
2026-05-13 14:45:44 +0300

    order step1
    Signed-off-by: Sidorov Vitaly <vit_s71@yahoo.com>

--- src/app/views/order/order-module.ts
+++ src/app/views/order/order-module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { OrderRoutingModule } from './order-routing-module';
 import { Cart } from './cart/cart';
 import { Order } from './order/order';
